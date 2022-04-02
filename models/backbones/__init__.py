471f0b680f3ad61235a0d7a0b91a3e6cfd5c8e27
Jian Zhang <jianzhangcs@aliyun.com>
2022-04-02 17:06:35 +0800

    Delete models directory

--- models/backbones/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-
-from .panini import Panini_MFR, Panini_SR
-from .rrdb_net import RRDBNet
-
-
-__all__ = [
-    'RRDBNet', 'Panini_MFR', 'Panini_SR'
-]
--- models/components/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-
-from .stylegan2 import StyleGAN2Discriminator, StyleGANv2Generator
-
-__all__ = [
-    'StyleGAN2Discriminator', 'StyleGANv2Generator'
-]
