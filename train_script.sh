ce422ee762373cb21814c1d153816746f210143f
Jian Zhang <jianzhangcs@aliyun.com>
2022-04-02 18:54:33 +0800

    Update train_script.sh

--- train_script.sh
+++ train_script.sh
@@ -7,5 +7,5 @@ python tools/train.py configs/panini_mfr.py --work-dir my_project/panini_mfr --r
 python tools/train.py configs/panini_sr.py --work-dir my_project/panini_sr --resume-from my_project/panini_sr/latest.pth
 
 # Train with multiple GPUs
-python -m torch.distributed.launch --nproc_per_node=2 tools/train.py configs/panini_mfr.py --work-dir my_project/panini_mfr
-python -m torch.distributed.launch --nproc_per_node=2 tools/train.py configs/panini_sr.py --work-dir my_project/panini_sr
+python -m torch.distributed.launch --nproc_per_node=2 tools/train.py configs/panini_mfr.py --work-dir my_project/panini_mfr --launcher pytorch
+python -m torch.distributed.launch --nproc_per_node=2 tools/train.py configs/panini_sr.py --work-dir my_project/panini_sr --launcher pytorch
